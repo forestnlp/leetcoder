9dbfa490e61d4264d6dbf3b9075f4ddc43d28152
user <user@LAPTOP-A8PE8997>
2020-05-14 16:47:19 +0800

    q94

--- src/com/leetcode/tree/Test.java
+++
@@ -1,5 +0,0 @@
-package com.leetcode.tree;
-
-public class Test {
-
-}
--- src/com/leetcode/tree/q94/Solution.java
+++ src/com/leetcode/tree/q94/Solution.java
@@ -1,5 +1,22 @@
 package com.leetcode.tree.q94;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.leetcode.tree.TreeNode;
+
+
+public class Solution {
+	public List<Integer> inorderTraversal(TreeNode root) {
+    	List<Integer> travels = new ArrayList<Integer>();
+    	helper(root,travels);
+    	return travels;
+    }
+    
+    public void helper(TreeNode root,List<Integer> travels) {
+    	if(root==null) return;
+    	helper(root.left,travels);
+    	travels.add(root.val);
+    	helper(root.right,travels);
+    }
 }
